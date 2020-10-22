71f4dcfee8d6bb0dc48dc442e8dd1970448a0431
slashkoopa <49096720+slashkoopa@users.noreply.github.com>
2020-10-22 23:40:14 +0800

    Revert "Revert "test""
    This reverts commit a62ff44af3679a070e40cc17ee919ef3edba05a5.

--- app/src/main/java/com/example/cs128_project/MainActivity.java
+++ app/src/main/java/com/example/cs128_project/MainActivity.java
@@ -18,5 +18,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, Game_Activity.class);
         startActivity(intent);
         //test changes
+        //test changes2
     }
 }
