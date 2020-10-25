5af73d7712f2613ab4613d5350819c799a79ff16
frankyprog08 <73292776+frankyprog08@users.noreply.github.com>
2020-10-25 17:55:57 +0800

    Main Menu
    moved customization and rounds to menu, added onclcick listeners to color buttons

--- app/src/main/java/com/example/cs128_project/MainActivity.java
+++ app/src/main/java/com/example/cs128_project/MainActivity.java
@@ -5,8 +5,19 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    private static final int[] idArray = {R.id.red, R.id.green, R.id.blue, R.id.brown, R.id.red2, R.id.green2, R.id.blue2, R.id.brown2};
+
+    private Button[] roundButton = new Button[idArray.length];
+
+    int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +33,54 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Spinner rounds = findViewById(R.id.rounds);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.rounds, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        rounds.setAdapter(adapter);
+        rounds.setOnItemSelectedListener(this);
+
+        for(i = 0; i<idArray.length; i++){
+            roundButton[i] = (Button)findViewById(idArray[i]);
+
+            roundButton[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    switch(v.getId()){
+                        case R.id.red:
+                            Toast.makeText(getApplicationContext(), "Player 1 changed color to red", Toast.LENGTH_SHORT).show();
+                            break;
+
+                        case R.id.green:
+                            Toast.makeText(getApplicationContext(), "Player 1 changed color to green", Toast.LENGTH_SHORT).show();
+                            break;
+
+                        case R.id.blue:
+                            Toast.makeText(getApplicationContext(), "Player 1 changed color to blue", Toast.LENGTH_SHORT).show();
+                            break;
+
+                        case R.id.brown:
+                            Toast.makeText(getApplicationContext(), "Player 1 changed color to brown", Toast.LENGTH_SHORT).show();
+                            break;
+
+                        case R.id.red2:
+                            Toast.makeText(getApplicationContext(), "Player 2 changed color to red", Toast.LENGTH_SHORT).show();
+                            break;
+
+                        case R.id.green2:
+                            Toast.makeText(getApplicationContext(), "Player 2 changed color to green", Toast.LENGTH_SHORT).show();
+                            break;
+
+                        case R.id.blue2:
+                            Toast.makeText(getApplicationContext(), "Player 2 changed color to blue", Toast.LENGTH_SHORT).show();
+                            break;
+
+                        case R.id.brown2:
+                            Toast.makeText(getApplicationContext(), "Player 2 changed color to brown", Toast.LENGTH_SHORT).show();
+                            break;
+                    }
+                }
+            });
+        }
     }
 
     public void startGame(View view) {
@@ -29,5 +88,17 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
         //test changes
         //test changes2
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
